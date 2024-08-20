b92ec13ac9bd2aa6cb11e7ab366c6fe39b86b263
Zuocheng Dai <zdai7774@uni.sydney.edu.au>
2024-08-20 13:29:38 +0800

    generally finished

--- q2_clustering.py
+++ q2_clustering.py
@@ -1,4 +1,12 @@
-
+import re
+import glob
+import numpy as np
+import pandas as pd
+import skfda
+import matplotlib.pyplot as plt
+from fdasrsf import pairwise_align_functions
+from skfda.ml.clustering import KMeans
+from skfda.exploratory.visualization.clustering import ClusterPlot
 
 from utils import load_data, check_folder, data_after_align
 
@@ -7,8 +15,127 @@ check_folder()
 
 ########################################################
 
-# Load the data
-target_dict, data_dict = load_data('./data/')
-# Get data after alignemnt
-obs_data_dict, aligned_data_dict, warp_func_dict = data_after_align(target_dict, data_dict)
+# read in targets
+targets = pd.read_csv('./data/target_music.csv')
+target_dict = dict()
+for i in range(1, targets.shape[1]):
+    target = targets.iloc[:, i].values
+    target_dict[i] = target[~np.isnan(target)]
+
+
+# read in data
+file_paths = glob.glob('./data/music*.csv')
+# 使用正则表达式从每个路径中提取数字
+def extract_number(path):
+    match = re.search(r'\d+', path)
+    if match:
+        return int(match.group())
+    return None  # 对于没有数字的路径，返回None
+# 对路径列表进行排序，排序键为路径中的数字
+file_paths = sorted(file_paths, key=extract_number)
+pattern = re.compile(r'music(\d+)\.csv')
+
+normal_data_clust = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 1, 7: 1, 8: 0, 9: 1, 10: 0, 11: 1, 12: 1}
+normal_warp_clust = {1: 0, 2: 1, 3: 0, 4: 1, 5: 1, 6: 0, 7: 0, 8: 1, 9: 1, 10: 1, 11: 1, 12: 0}
+
+all_id = pd.read_csv(file_paths[0]).iloc[:, 1].values
+# Initialize dict to store clustering results
+aligned_data_clust = {ID: list() for ID in all_id}
+warp_func_clust = {ID: list() for ID in all_id}
+for path in file_paths:
+    # get music id
+    match = pattern.search(path)
+    idx = int(match.group(1))
+    df = pd.read_csv(path).dropna()
+    valid_id = df.iloc[:, 1].values
+
+    # then, extract each line, subtract with the target
+    diff_aligned_data, diff_warp_func = list(), list()
+    for index, row in df.iterrows():
+        row_data = row.iloc[2:].values.astype(float)
+        t = np.linspace(0, 1, row_data.shape[0])
+        aligned_data, warp_func = pairwise_align_functions(target_dict[idx], row_data, t, omethod='DP2')[:2]
+        # Get the absolute diff of aligned func
+        diff_func_data = abs(aligned_data - target_dict[idx])
+        diff_aligned_data.append(diff_func_data)
+        # Get the absolute diff of warp func
+        diff_warp_data = abs(warp_func - t)
+        diff_warp_func.append(diff_warp_data)
+
+    # 然后直接做clustering了
+    # transfrom data to skfda data
+    dat_diff_fd = skfda.FDataGrid(data_matrix=diff_aligned_data, grid_points=t)
+    warp_diff_fd = skfda.FDataGrid(data_matrix=diff_warp_func, grid_points=t)
+
+    n_clusters = 2
+    seed = 5003
+    # 首先先做aligned data的clustering
+    dat_diff_kmeans = KMeans(n_clusters=n_clusters, random_state=seed)
+    dat_diff_kmeans.fit(dat_diff_fd)
+    dat_diff_group = dat_diff_kmeans.predict(dat_diff_fd)
+    # visualization of the clustering
+    ClusterPlot(dat_diff_kmeans, dat_diff_fd).plot()
+    plt.tight_layout()
+    plt.savefig(f'./output/plot/music{idx}/music{idx}_data_diff_clust.png', dpi=300)
+    plt.close('all')
+
+    data_diff_pred = {ID: pred for ID, pred in zip(valid_id, dat_diff_group)}
+    for key in aligned_data_clust.keys():
+        if key in valid_id:
+            if data_diff_pred[key] == normal_data_clust[idx]:
+                aligned_data_clust[key].append(0)
+            else:
+                aligned_data_clust[key].append(1)
+        else:
+            aligned_data_clust[key].append(np.nan)
+
+    # 然后再给warping function做clustering
+    warp_diff_kmeans = KMeans(n_clusters=n_clusters, random_state=seed)
+    warp_diff_kmeans.fit(warp_diff_fd)
+    warp_diff_group = warp_diff_kmeans.predict(warp_diff_fd)
+
+    # visualization of the clustering
+    ClusterPlot(warp_diff_kmeans, warp_diff_fd).plot()
+    plt.tight_layout()
+    plt.savefig(f'./output/plot/music{idx}/music{idx}_warp_diff_clust.png', dpi=300)
+    plt.close('all')
+
+    warp_diff_pred = {ID: pred for ID, pred in zip(valid_id, warp_diff_group)}
+    for key in warp_func_clust.keys():
+        if key in valid_id:
+            if warp_diff_pred[key] == normal_warp_clust[idx]:
+                warp_func_clust[key].append(0)
+            else:
+                warp_func_clust[key].append(1)
+        else:
+            warp_func_clust[key].append(np.nan)
+# Finish doing the clustering
+def get_emotion(idx):
+    if idx in [1, 2, 3]:
+        return 'anger'
+    elif idx in [4, 5, 6]:
+        return 'happiness'
+    elif idx in [7, 8, 9]:
+        return 'sadness'
+    elif idx in [10, 11, 12]:
+        return 'tenderness'
+# Generate a dataframe to save the data
+pred_df = []
+for ID in all_id:
+    for i, pred in enumerate(zip(aligned_data_clust[ID], warp_func_clust[ID])):
+        amp_pred, pha_pred = pred
+        row_data = {'ID': ID, 'Emotion': get_emotion(i + 1), 'Piece': i + 1, 'amp_pred': amp_pred, 'pha_pred': pha_pred}
+        pred_df.append(row_data)
+pred_df = pd.DataFrame(pred_df).dropna()
+pred_df.to_csv(f'./output/dataframe/clust_pred_results.csv', index=False)
+
+# # 假设data是包含两个分类变量的DataFrame
+# from scipy.stats import chi2_contingency
+# table = pd.crosstab(pred_df['amp_pred'], pred_df['pha_pred'])
+#
+# # 执行卡方检验
+# chi2, p, dof, expected = chi2_contingency(table)
+# print("Chi-square test statistic:", chi2)
+# print("p-value:", p)
+
 
