123610b3077d0157af3e3fec3a40accb17e504a9
Zuocheng Dai <zdai7774@uni.sydney.edu.au>
2024-09-09 17:56:49 +0800

    add comparison for amp and pha dist

---
+++ compare.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+amp_pha_dist_a = pd.read_csv('./alignment/output/dataframe/amp_pha_dist_summ_stat.csv')
+amp_pha_dist_na = pd.read_csv('./no_alignment/output_na/dataframe_na/amp_pha_dist_summ_stat_na.csv')
+amp_pha_dist_a['align'] = 'aligned'
+amp_pha_dist_na['align'] = 'misaligned'
+amp_pha_dist = pd.concat([amp_pha_dist_na, amp_pha_dist_a], ignore_index=True)
+
+# 设置全局字体大小
+plt.rcParams.update({'font.size': 18})  # 将 14 替换为你想要的字体大小
+for aspect in ['amp', 'pha']:
+    fig, axes = plt.subplots(3, 4, figsize=(19, 10))
+    # 确保 axes 是一维数组，方便遍历
+    axes = axes.flatten()
+    for i, ax in enumerate(axes):
+        amp_pha_dist_idx = amp_pha_dist[amp_pha_dist['id'] == i + 1]
+        ax.errorbar(
+            amp_pha_dist_idx['align'],
+            amp_pha_dist_idx[f'{aspect}_mean'],
+            yerr=amp_pha_dist_idx[f'{aspect}_std'],
+            fmt='o',
+            capsize=15
+        )
+        ax.set_xlim([-1, 2])
+        ax.set_title(f'Music {i + 1}')
+
+    if aspect == 'amp':
+        plt.suptitle('Amplitude Distance Summary Statistics Comparison', fontsize=20)
+    elif aspect == 'pha':
+        plt.suptitle('Phase Distance Summary Statistics Comparison', fontsize=20)
+    plt.tight_layout()
+    plt.show()
+    plt.close('all')
+
+
+
+
+
+
+
