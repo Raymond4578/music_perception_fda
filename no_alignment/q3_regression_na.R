9b1948667c14e6323e62637045606a3438e30513
Zuocheng Dai <zdai7774@uni.sydney.edu.au>
2024-09-10 17:58:54 +0800

    finish edit q3 for misaligned in R

---
+++ no_alignment/q3_regression_na.R
@@ -0,0 +1,20 @@
+rm(list = ls())
+# Load the dataset
+dat <- read.csv('./no_alignment/output_na/dataframe_na/amp_pha_dist_reg_na.csv')
+dat$Emotion <- factor(dat$Emotion, levels = c('anger', 'happiness', 'sadness', 'tenderness'))
+dat$Piece <- factor(dat$Piece)
+dat$ID <- factor(dat$ID)
+
+# Construct a mixture effect model: amplitude distance ~ Emotion
+amp_m <- lmerTest::lmer(
+  Amp_dist ~ Emotion + (1 | ID),
+  data = dat
+)
+summary(amp_m)
+
+# Construct a mixture effect model: phase distance ~ Emotion
+pha_m <- lmerTest::lmer(
+  Pha_dist ~ Emotion + (1 | ID),
+  data = dat
+)
+summary(pha_m)
