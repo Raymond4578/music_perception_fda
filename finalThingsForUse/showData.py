74771dd1500c1e9094890bdc6030a0469a9ad520
Zuocheng Dai <zdai7774@uni.sydney.edu.au>
2025-03-02 18:29:27 +0000

    Draw the data illustration plot.

---
+++ finalThingsForUse/showData.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from utils import load_data
+
+output_path = './output'
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+music_idx = 3
+
+# Load the data
+target_dict, data_dict = load_data('../data/')
+# t = np.linspace(0, 1, len(target_dict[music_idx]))
+t = np.arange(8, 8 + 2 * len(target_dict[music_idx]), 2)
+
+###############################################################
+# illustration for target and observed responses in section 1.2
+###############################################################
+
+target = target_dict[music_idx]
+obs_data_df = data_dict[music_idx]
+# Drop first two column about idx and ID
+obs_data_df.drop(obs_data_df.columns[:2], axis=1, inplace=True)
+obs_data = np.array(obs_data_df).astype(float)
+obs_data_mean = np.mean(obs_data, axis=0)
+
+plt.figure(figsize=(8, 3.5))
+for i, response in enumerate(obs_data):
+    if i == 0:
+        plt.plot(t, response, linewidth=0.2, color='black', linestyle='--', label="Ovservations")
+    else:
+        plt.plot(t, response, linewidth=0.2, color='black', linestyle='--')
+plt.plot(t, target, linewidth=2, color='blue', linestyle='-', label="Target")
+plt.plot(t, obs_data_mean, linewidth=2, color='red', linestyle='-', label="Pointwise Mean")
+plt.legend(bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0.)
+plt.xlabel('$t$')
+plt.locator_params(axis='x', nbins=10)
+plt.tight_layout()
+plt.savefig('./output/showData.png', dpi=300)
+
